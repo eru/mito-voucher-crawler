8eddbc7a02ed259db4300cbd35022348429cfef7
Kazuki Hashimoto <eru.tndl@gmail.com>
2020-11-01 12:50:34 +0900

    :tada: 2020-11-01のデータに更新

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const writeFile = (file, data) => fs.writeFile(`output/${file}`, data, err => {
   if (err) throw err
   console.log(`${file} writed.`)
 })
-const last_page = 87
+const last_page = 88
 
 ;(async () => {
   const browser = await puppeteer.launch()
